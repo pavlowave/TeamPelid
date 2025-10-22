041e58d60a2fce9ae84ed72955dbe97638a54ea6
Pavel Sidorov <52633235+sxwiix@users.noreply.github.com>
2025-10-22 18:32:15 +0300

    упрощенное добавление фотографий

--- modules/locations/admin.py
+++ modules/locations/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place, PlaceImage
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+    fields = ('image', 'is_main')
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'latitude', 'longitude', 'image')
+    list_display = ('name', 'latitude', 'longitude')
+    inlines = [PlaceImageInline]
--- modules/locations/models.py
+++ modules/locations/models.py
@@ -5,7 +5,23 @@ class Place(models.Model):
     description = models.TextField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    image = models.ImageField(upload_to='places/')
+    image = models.ImageField(upload_to='places/', blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='places/')
+    is_main = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.is_main:
+
+            PlaceImage.objects.filter(place=self.place, is_main=True).exclude(pk=self.pk).update(is_main=False)
+            self.place.image = self.image
+            self.place.save()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.place.name} - изображение"
